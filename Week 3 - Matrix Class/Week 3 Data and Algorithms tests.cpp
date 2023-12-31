ce313ffe5a4f6fc32700889e3bd2a7ebba58410c
Timofei Head <125971354+Thead12@users.noreply.github.com>
2023-12-31 18:59:11 +0000

    Update and rename Week 3 Data and Algorithms.cpp to Week 3 Data and Algorithms tests.cpp

---
+++ Week 3 - Matrix Class/Week 3 Data and Algorithms tests.cpp
@@ -0,0 +1,44 @@
+// Various tests for Week 3 Data and Algorithms
+
+#include <iostream>
+#include <vector>
+
+//Uncomment to remove debug
+//#define NDEBUG
+#include <cassert>
+
+#include "matrix.h"
+
+int main()
+{
+
+    vector<vector<double> > arr1 = {
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    vector<vector<double> > arr2 = {
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    Dense::Matrix matrix1(8, 8, 5.7);
+
+    Dense::Matrix matrix2(8, 8, 6.989);
+
+    Dense::Matrix matrix3 = matrix2.conquer(matrix3);
+
+    matrix3.print();
+}
