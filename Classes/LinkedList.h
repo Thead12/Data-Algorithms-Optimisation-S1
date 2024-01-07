cd71daf680570d665d61214003506ed441f00135
Timofei Head <125971354+Thead12@users.noreply.github.com>
2024-01-07 11:44:14 +0000

    Update LinkedList.h
    cleaned up code

--- Classes/LinkedList.h
+++ Classes/LinkedList.h
@@ -10,8 +10,6 @@
 //#define NDEBUG
 //#include<cassert>
 
-using namespace std;
-
 //Node class
 class Node
 {
@@ -20,17 +18,9 @@ public: //public methods
     Node* next; //Pointer to next node
 
     //==================== Constructors ==================
-    Node()
-    {
-        value = 0.0;
-        next = NULL;
-    }
+    Node(); // Default constructor for Node
 
-    Node(double data)
-    {
-        value = data;
-        next = NULL;
-    }
+    Node(double data); // Constructor for Node with given data
 };
 
 class LinkedList {
@@ -40,224 +30,207 @@ private:
 public:
 
     //==================== Constructors ==================
-    LinkedList()
-    {
-        //Node();
-        head = NULL;
-        length = 0;
-    }
+    LinkedList(); // Default constructor for LinkedList
 
     //Getters ==================== Setters 
-    double get(int index)
-    {
-        assert(("Index not valid.", 0 <= index && index <= length));
+    double& get(int index); // Getter for retrieving data at a given index
+    void set(int index, double data); // Setter for updating data at a given index
+
+    void DeleteNode(int index); // Function to delete a node at a given index
+    void InsertNode(int index, double col, double data); // Function to insert a node at a given index with specific data
+    void InsertBehind(int index, double data); // Function to insert a node behind a given index with specific data
+    void InsertForward(int index, double data); // Function to insert a node in front of a given index with specific data
+    void InsertHead(double data); // Function to insert a node at the head with specific data
+    void InsertTail(double data); // Function to insert a node at the tail with specific data
+    void PrintList(); // Function to print the linked list
+};
+
+// Node Class
+
+// Default constructor for Node
+Node::Node()
+{
+    value = 0.0;
+    next = nullptr;
+}
+
+// Constructor for Node with given data
+Node::Node(double data)
+{
+    value = data;
+    next = nullptr;
+}
+
+
+// LinkedList Class
+
+// Default constructor for LinkedList
+LinkedList::LinkedList()
+{
+    head = nullptr;
+    length = 0;
+}
+
+// Getter for retrieving data at a given index
+double& LinkedList::get(int index)
+{
+    assert(("Index not valid.", 0 <= index && index <= length));
+    Node* temp = head;
+    for (int i = 0; i < index; i++) {
+        temp = temp->next;
+    }
+    return temp->value;
+}
+
+// Setter for updating data at a given index
+void LinkedList::set(int index, double data)
+{
+    assert(("Index not valid.", 0 <= index && index <= length - 1));
+    Node* temp = head;
+    for (int i = 0; i < index; i++) {
+        temp = temp->next;
+    }
+    temp->value = data;
+}
+
+// Function to delete a node at a given index
+void LinkedList::DeleteNode(int index)
+{
+    assert(("Index not valid.", 0 <= index && index <= length - 1));
+    if (index == 0) {
         Node* temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp->next;
-        }
-        return temp->value;
+        head = head->next;
+        delete temp;
+        length--;
+        return;
     }
-
-    void set(int index, double data)
-    {
-        assert(("Index not valid.", 0 <= index && index <= length - 1));
-        Node* temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp->next;
+    else {
+        Node* temp1 = head;
+        Node* temp2 = head->next;
+        for (int i = 0; i < index - 1; i++) {
+            temp1 = temp2;
+            temp2 = temp2->next;
         }
-        temp->value = data;
+        temp1->next = temp2->next;
+        delete temp2;
+        length--;
+        return;
     }
+}
 
-    void DeleteNode(int index)
-    {
-        assert(("Index not valid.", 0 <= index && index <= length - 1));
-        //Delete at head
-        if (index == 0) {
-
-            Node* temp = head;
-            //Assign next node as head
-            head = head->next;
-            //Delete old head node
-            delete[] temp;
-
-            length--;
-            return;
-        }
-        else {
-            //Traverse till index
-            Node* temp1 = head;
-            Node* temp2 = head->next;
-
-            //Traverse till indexx
-            for (int i = 0; i < index - 1; i++) {
-                temp1 = temp2;
-                temp2 = temp2->next;
-            }
-
-            temp1->next = temp2->next;
-
-            delete temp2;
-
-            length--;
-            return;
-        }
-    }
-
-    void InsertNode(int index, double col, double data)
-    {
-        assert(("Index not valid.", 0 <= index && index <= length - 1));
-        //Create new Node
-        Node* newNode = new Node(data);
-
-        //If list is empty
-        if (head == NULL) {
-            head = newNode;
-
-            length++;
-            return;
-        }
-        else {
-            //Traverse till index
-            Node* temp = head;
-            for (int i = 0; i < index; i++) {
-                temp = temp->next;
-            }
-
-            //Insert at index
-            newNode->next = temp->next;
-            temp->next = newNode;
-
-            length++;
-            return;
-        }
-    }
-
-    //Inserts new node behind given index
-    void InsertBehind(int index, double data)
-    {
-        //Create new Node
-        Node* newNode = new Node(data);
-
-        //If list is empty
-        if (head == NULL) {
-            head = newNode;
-
-            length++;
-            return;
-        }
-        else if (index == 0) { //Edge case where insertion point is at the start of the LinkedList
-            InsertHead(data);
-        }
-        else {
-            //Traverse till index
-            Node* temp = head;
-            for (int i = 0; i < index - 1; i++) {
-                temp = temp->next;
-            }
-
-            //Insert at index
-            newNode->next = temp->next;
-            temp->next = newNode;
-
-            length++;
-            return;
-        }
-    }
-
-    //Inserts new node infront of given index
-    void InsertForward(int index, double data) {
-        //Creat new node
-        Node* newNode = new Node(data);
-
-        //If list if empty
-        if (head == NULL) {
-            head = newNode;
-
-            length++;
-        }
-        else if (index == length) { //Edge case where the insertion point is at the end of the LinkedList
-            InsertTail(data);
-        }
-        else {
-            //Traverse till index
-            Node* temp = head;
-            for (int i = 0; i < index; i++) {
-                temp = temp->next;
-            }
-            //Insert at index
-            newNode->next = temp->next;
-            temp->next = newNode;
-
-            length++;
-        }
-    }
-
-    void InsertHead(double data)
-    {
-        //Create new node
-        Node* newNode = new Node(data);
-
-        //If list is empty
-        if (head == NULL) {
-            head = newNode;
-
-            length++;
-            return;
-        }
-
-        //Make new node point to head
-        newNode->next = head;
-
-        //Make new node into head
+// Function to insert a node at a given index with specific data
+void LinkedList::InsertNode(int index, double col, double data)
+{
+    assert(("Index not valid.", 0 <= index && index <= length - 1));
+    Node* newNode = new Node(data);
+    if (head == nullptr) {
         head = newNode;
-
         length++;
         return;
     }
-
-    void InsertTail(double data)
-    {
-        //Create new Node
-        Node* newNode = new Node(data);
-
-        //If list is empty
-        if (head == NULL) {
-            head = newNode;
-
-            length++;
-            return;
-        }
-        else {
-            //Traverse till end of list
-            Node* temp = head;
-            while (temp->next != NULL) {
-
-                //Update temp
-                temp = temp->next;
-            }
-
-            //Insert at the last
-            temp->next = newNode;
-
-            length++;
-            return;
-        }
-
-    }
-
-    void PrintList()
-    {
+    else {
         Node* temp = head;
-
-        //Check for empty list
-        if (head == NULL) {
-            cout << "List empty" << endl;
-            return;
-        }
-
-        while (temp != NULL) {
-            cout << temp->value << endl;
+        for (int i = 0; i < index; i++) {
             temp = temp->next;
         }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        length++;
+        return;
     }
-};
+}
+
+// Function to insert a node behind a given index with specific data
+void LinkedList::InsertBehind(int index, double data)
+{
+    Node* newNode = new Node(data);
+    if (head == nullptr) {
+        head = newNode;
+        length++;
+        return;
+    }
+    else if (index == 0) {
+        InsertHead(data);
+    }
+    else {
+        Node* temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        length++;
+        return;
+    }
+}
+
+// Function to insert a node in front of a given index with specific data
+void LinkedList::InsertForward(int index, double data)
+{
+    Node* newNode = new Node(data);
+    if (head == nullptr) {
+        head = newNode;
+        length++;
+    }
+    else if (index == length) {
+        InsertTail(data);
+    }
+    else {
+        Node* temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        length++;
+    }
+}
+
+// Function to insert a node at the head with specific data
+void LinkedList::InsertHead(double data)
+{
+    Node* newNode = new Node(data);
+    if (head == nullptr) {
+        head = newNode;
+        length++;
+        return;
+    }
+    newNode->next = head;
+    head = newNode;
+    length++;
+    return;
+}
+
+// Function to insert a node at the tail with specific data
+void LinkedList::InsertTail(double data)
+{
+    Node* newNode = new Node(data);
+    if (head == nullptr) {
+        head = newNode;
+        length++;
+        return;
+    }
+    else {
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        length++;
+        return;
+    }
+}
+
+// Function to print the linked list
+void LinkedList::PrintList()
+{
+    Node* temp = head;
+    if (head == nullptr) {
+        std::cout << "List empty" << std::endl;
+        return;
+    }
+    while (temp != nullptr) {
+        std::cout << temp->value << std::endl;
+        temp = temp->next;
+    }
+}
