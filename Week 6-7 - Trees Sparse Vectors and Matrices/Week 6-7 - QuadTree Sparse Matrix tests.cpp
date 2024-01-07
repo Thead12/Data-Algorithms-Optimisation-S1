8da7ecdb08145b291bedd8d696bd1ce71b676251
Timofei Head <125971354+Thead12@users.noreply.github.com>
2024-01-07 15:35:59 +0000

    Update Week 6-7 - QuadTree Sparse Matrix tests.cpp
    Made test more extensive

--- Week 6-7 - Trees Sparse Vectors and Matrices/Week 6-7 - QuadTree Sparse Matrix tests.cpp
+++ Week 6-7 - Trees Sparse Vectors and Matrices/Week 6-7 - QuadTree Sparse Matrix tests.cpp
@@ -1,54 +1,85 @@
-// Week 6-7 - Trees Sparse Vectors and Matrices.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//============== Required includes ============== 
 #include <iostream>
 #include<vector>
-#include<algorithm>
 
 //Uncomment to remove debug
 //#define NDEBUG
 #include<cassert>
 
+//============== Dependent headerfiles ==============
 #include "matrix.h"
-#include "LinkedListSparseMatrix.h"
-#include "CSRSparseMatrix.h"
-#include "SparseVector.h"
 #include "QuadTreeSparseMatrix.h"
 
-void LineBreak() {
-    std::cout << '\n';
+void testEmptyMatrix() {
+    QuadTree::SparseMatrix<int> emptyMatrix(4, 4);
+
+    assert(emptyMatrix.getNumRows() == 4);
+    assert(emptyMatrix.getNumCols() == 4);
+
+    for (int i = 0; i < emptyMatrix.getNumRows(); ++i) {
+        for (int j = 0; j < emptyMatrix.getNumCols(); ++j) {
+            assert(emptyMatrix.getValue(i, j) == 0);
+        }
+    }
 }
 
-void Print(std::string message) {
-    std::cout << message << std::endl;
+void testInitializationFromVector() {
+    std::vector<std::vector<int>> vectorMatrix = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16} };
+    QuadTree::SparseMatrix<int> fromVector(vectorMatrix);
+
+    assert(fromVector.getNumRows() == 4);
+    assert(fromVector.getNumCols() == 4);
+
+    for (int i = 0; i < fromVector.getNumRows(); ++i) {
+        for (int j = 0; j < fromVector.getNumCols(); ++j) {
+            assert(fromVector.getValue(i, j) == vectorMatrix[i][j]);
+        }
+    }
 }
 
-template<typename T>
-void Print(T message) {
-    std::cout << message << std::endl;
+void testInitializationFromDenseMatrix() {
+    Dense::Matrix<int> denseMatrix(4, 4, 1);
+    QuadTree::SparseMatrix<int> fromDense(denseMatrix);
+
+    assert(fromDense.getNumRows() == 4);
+    assert(fromDense.getNumCols() == 4);
+
+    for (int i = 0; i < fromDense.getNumRows(); ++i) {
+        for (int j = 0; j < fromDense.getNumCols(); ++j) {
+            assert(fromDense.getValue(i, j) == 1);
+        }
+    }
 }
 
+void testSetValueAndGet() {
+    QuadTree::SparseMatrix<int> testMatrix(4, 4);
 
-int main() {
+    testMatrix.setValue(1, 1, 42);
+    assert(testMatrix.getValue(1, 1) == 42);
 
-    std::vector<std::vector<int>> vector =
-    {
-        {0, 0, 0, 0},
-        {1, 0, 4, 0},
-        {0, 8, 5, 0},
-        {0, 0, 0, 0}
-    };
+    testMatrix.setValue(0, 0, 10);
+    testMatrix.setValue(2, 2, 20);
 
-    QuadTree::SparseMatrix<int> myMatrix(vector);
-
-    myMatrix.setValue(0, 0, 2);
-
-    Print(myMatrix.getValue(0, 0));
-
-    LineBreak();
-
-    myMatrix.printDense();
-
-    return 0;
+    assert(testMatrix.getValue(0, 0) == 10);
+    assert(testMatrix.getValue(1, 1) == 42);
+    assert(testMatrix.getValue(2, 2) == 20);
 }
 
+void testOutOfBounds() {
+    QuadTree::SparseMatrix<int> testMatrix(4, 4);
+
+    // Attempt to set value out of bounds should not affect the matrix
+    testMatrix.setValue(4, 4, 100);
+    assert(testMatrix.getValue(4, 4) == 0);
+}
+
+int main()
+{
+    testEmptyMatrix();
+    testInitializationFromVector();
+    testInitializationFromDenseMatrix();
+    testSetValueAndGet();
+    //testOutOfBounds(); When uncommented, correct index out of bounds error is thrown
+
+    std::cout << "All tests passed!" << std::endl;
+}
